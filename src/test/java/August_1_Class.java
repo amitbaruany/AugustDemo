dd59b0bc7da12e78035821e851f726402ed1cfef
Amit <amitbaruany@gmail.com>
2020-08-25 19:51:51 -0400

    Dev 1 created branch and edited and pushing

--- src/test/java/August_1_Class.java
+++ src/test/java/August_1_Class.java
@@ -5,5 +5,7 @@ public class August_1_Class {
     // This is August 1 pulled, edited and pushing 12.40
     // This is August 2 pulled, edited and pushing 12.45
     // Hello 7.44pm
+    // So Now Dev 1 just created a dev branch and edited and pushing to dev branch
+    
 
 }
